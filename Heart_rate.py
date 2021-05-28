961b5097c9b8d8eb42abc3ac7d949603b5766113
HARISH B <72798342+HARISH-B-2001@users.noreply.github.com>
2021-05-28 19:17:25 +0530

    Update Heart_rate.py

--- Heart_rate.py
+++ Heart_rate.py
@@ -3,6 +3,7 @@ import imageio
 import matplotlib.pyplot as plt
 
 fps = 30
+
 file_path = input('Enter the file path:')
 
 #POST PROCESSING
@@ -10,35 +11,35 @@ file_path = input('Enter the file path:')
 video = imageio.get_reader(file_path, 'ffmpeg')
 colors = {'red':[],'green':[],'blue':[]}
 for frame in video:
-    lumped_pixel = np.mean(frame,axis=(0,1))
-    colors['red'].append(lumped_pixel[0])
-    colors['green'].append(lumped_pixel[1])
-    colors['blue'].append(lumped_pixel[2])
+    pixel = np.mean(frame,axis=(0,1))
+    colors['red'].append(pixel[0])
+    colors['green'].append(pixel[1])
+    colors['blue'].append(pixel[2])
 for key in colors:
     colors[key] = np.divide(colors[key],255)
 x = np.arange(len(colors['red'])) / fps
 
 #FILTTERING DATA
 
-colors['red_filt'] = list() 
-colors['red_filt'] = np.append(colors['red_filt'], colors['red'][0])
+colors['red_filter'] = list() 
+colors['red_filter'] = np.append(colors['red_filter'], colors['red'][0])
 tau = 0.25
-fsample = fps
-alpha = tau / (tau + 2/fsample)
+fps_sample = fps
+alpha = tau / (tau + 2/fps_sample)
 for index, frame in enumerate(colors['red']): 
     if index > 0:
-        y_prev= colors['red_filt'][index - 1]
+        y_prev= colors['red_filter'][index - 1]
         x_curr= colors['red'][index]
         x_prev= colors['red'][index - 1]
-        colors['red_filt'] = np.append(colors['red_filt'], alpha*(y_prev + x_curr - x_prev))
+        colors['red_filter'] = np.append(colors['red_filter'], alpha*(y_prev + x_curr - x_prev))
 x_filt = x[50:-1]
-colors['red_filt'] = colors['red_filt'][50:-1]
+colors['red_filter'] = colors['red_filter'][50:-1]
 
 #PERFORMING FFT ON DATA
 
-red_fft = np.absolute(np.fft.fft(colors['red_filt']))
-N = len(colors['red_filt'])
-freqs = np.arange(0,fsample/2,fsample/N)
+red_fft = np.absolute(np.fft.fft(colors['red_filter']))
+N = len(colors['red_filter'])
+freqs = np.arange(0,fps_sample/2,fps_sample/N)
 red_fft = red_fft[0:len(freqs)]
 
 #MEASURING HEARTRATE FROM FFT
